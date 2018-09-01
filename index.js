b6d91dd986dfd6f7278a3e0a4e93219c8af8e393
Paula Montiel <p_dany_mr@hotmail.com>
2018-08-31 23:55:46 -0300

    dying

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const marked = require('marked');
+const Marked = require('marked');
 const fs = require('fs');
 const fetch = require('node-fetch');
 const pathNode = require('path');
+const [, , ...userFile] = process.argv;
 var data;
+
 
 function validarArchivoODirectorio(path, options) {
   return new Promise((resolve, reject)=>{
@@ -60,37 +62,38 @@ function verLinks(data) {
 // (tomada desde biblioteca del mismo nombre y modificada para el ejercicio)
 // Recibe texto en markdown y retorna sus links en un arreglo
 function markdownLinkExtractor(markdown) {
-  const links = [];
+  var links = [];
 
-  const renderer = new Marked.Renderer();
+    var renderer = new marked.Renderer();
 
-  // Taken from https://github.com/markedjs/marked/issues/1279
-  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
+    // Taken from https://github.com/markedjs/marked/issues/1279
+    var linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
 
-  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
-  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
-  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
+    marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+    marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+    marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
+    
+    renderer.link = function (href, title, text) {
+        links.push(href);
+    };
+    renderer.image = function (href, title, text) {
+        // Remove image size at the end, e.g. ' =20%x50'
+        href = href.replace(/ =\d*%?x\d*%?$/, "");
+        links.push(href);
+    };
+    marked(markdown, { renderer: renderer });
 
-  renderer.link = function(href, title, text) {
-    links.push({
-      href: href,
-      text: text,
-      title: title,
-    });
-  };
-  renderer.image = function(href, title, text) {
-    // Remove image size at the end, e.g. ' =20%x50'
-    href = href.replace(/ =\d*%?x\d*%?$/, '');
-    links.push({
-      href: href,
-      text: text,
-      title: title,
-    });
-  };
-  Marked(markdown, {renderer: renderer});
-
-  return links;
+    return links;
 };
 // validarArchivoODirectorio('./md/text.md','');
 
-module.exports = validarArchivoODirectorio('./md/text.md', '');
+function getStatusCode(url){
+  var options = {
+    'muteHttpExceptions': true,
+    'followRedirects': false
+  };
+  var response = UrlFetchApp.fetch(url, options);
+  return response.getResponseCode();
+}
+
+module.exports = validarArchivoODirectorio('./README.md', '');
